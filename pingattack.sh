49be6c474c966c1424752bff6b6d83df016efdd8
suteaka1 <butterext+github@gmail.com>
2015-05-14 15:37:20 +0900

    Create pingattack.sh

---
+++ pingattack.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+#pingを使って同じネットワーク内で繋がるIPアドレスを調べる
+
+#所属するネットワークを抽出
+#ひとつひとつをなるべく簡素にした。
+#ipv6が必要になったときに簡単に変更できるようあえて1文で削らない。
+eth0=ip a | grep 'inet' | grep -v 'inet6' | grep -v 'eth1' | grep -v '127.0.0.1' | sed s/inet/inet:/ | cut -d: -f2 | awk '{ print $1}'
+eth0=$a.$b.$c.$d
+
+#192.168.20.10/24
+#だとしたら、第三オクテットまで取得して
+#以下でforループに使う
+#せっかくprefixもついてるので有効活用できると尚良い？
+
+#インターネットに繋がるかどうか確認(googleDNSに向けて)
+ping -c 5 8.8.8.8
+
+#これ以降をループさせる
+# 第４オクテット以外を、最初に抽出したものから引っ張りたい
+COUNT=0
+MAX_COUNT=255
+while [ $COUNT -lt $MAX_COUNT ]
+do
+        #echo "$COUNT"
+        COUNT=`expr $COUNT + 1`
+        ping -c 5 192.168.100.`echo "$COUNT"`
+done
+exit 0
