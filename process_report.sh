b5da619ec0483d8fc2e20a750c7d17e251a02fd4
suteaka1 <butterext+github@gmail.com>
2015-05-14 16:13:44 +0900

    Update process_report.sh

--- process_report.sh
+++ process_report.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
-#長時間動いているプロセス順にsort
-#time = 10番目のフィールド
-#⇒ sort -r -k -10
-
+#フィールドの種類
 #USER	
 #PID	
 #%CPU	
@@ -15,18 +12,18 @@
 #TIME	
 #COMMAND
 
-echo "長時間動いていたプロセス上位10を表示します"
-ps --no-header aux | sort -r -k 10 | head -n 10
+read -p "表示したいフィールド番号を1～11の中から選んでください。そのうちの上位10までを表示します: " field
+ps --no-header aux | sort -r -k $field | head -n 10
 
-# read -p "表示したいフィールドを選んでください。そのうちの上位10までを表示します: " field
-# ps --no-header aux | sort -r -k $field | head -n 10
-#
-
-echo "長時間動いていたプロセス下位10を表示します"
-ps --no-header aux | sort -r -k 10 | tail -n 10
-
+#長時間動いているプロセス順にsort
+#time = 10番目のフィールド
+#⇒ sort -r -k -10
+#echo "長時間動いていたプロセス上位10を表示します"
+#ps --no-header aux | sort -r -k 10 | head -n 10
+#echo "長時間動いていたプロセス下位10を表示します"
+#ps --no-header aux | sort -r -k 10 | tail -n 10
 #整形
-ps --no-header -e aux | sort -r -k 3 | head -n 20 | awk '{print $1,$3,$11;};'
+#ps --no-header -e aux | sort -r -k 3 | head -n 20 | awk '{print $1,$3,$11;};'
 
 ##今後の課題
 ##引数を使って条件を変更できるようにする
