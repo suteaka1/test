02c310429bcb4b6d66a66f8f6153ec16214e7346
suteaka1 <butterext+github@gmail.com>
2015-05-18 12:05:32 +0900

    Update pingattack.sh

--- pingattack.sh
+++ pingattack.sh
@@ -1,47 +1,5 @@
 #!/bin/sh
-## pingを使って同じネットワーク内で繋がるIPアドレスを調べ、自身の機器が所属するネットワークを抽出し
-## 他に繋がるデバイスが無いか調べる用途に使える。(もしかして：arp-scan)
-#
-## ひとつひとつをなるべく簡素にして、パイプの途中で区切ってその段階ではどういった処理を終えた段階なのか、
-## 把握できるようになるべく凝ったことをしないよう決めた。
-#
-###課題
-###・下のwhileループに第一、第二、第三オクテットを引き渡せるようにする
-###・pingして応答するかしないかを判断する条件式を付与する       -> sleepの必要性
-###・最後に結果を出力して正規表現で整形する？                   -> もっと高度なテキスト処理と、cronでまわすことで定期的なログを取れる？
-
-####パイプ間のコマンドの説明
-## ip a                 -> ifconfigなどで使われるnet-toolsは長い間メンテナンスされていなかったということで大変危険なので
-##                      (理由:http://iwashi.co/2014/08/07/reason-why-we-should-use-iproute2/ 
-##                      https://dougvitale.wordpress.com/2011/12/21/deprecated-linux-networking-commands-and-their-replacements/)
-##                      代用になるiproute2を使うことに。
-##                      また、ifconfigには:がipv4アドレスの手前にあったのでcutのデリミタで使えたが、ip addr showにはないので
-##                      せっかくなのでsedをつかって置換をする手順を加えてみた。
-#
-## grep 'inet'          -> ip aの標準出力から「inet」が含まれる行を抽出=ipアドレスの抽出
-## grep /24             -> ip aの標準出力から「/24」が含まれる行を抽出=第三オクテットまでを固定化したい。理由は下記でまた述べる。
-## grep -v 'inet6'      -> ip aからipv6が含まれる行を省く。
-## grep -v 'eth1'       -> ip aからeht1が含まれる行を省く。
-## grep -v '127.0.0.1'  -> ip aからローカルループバックアドレス(lo)が含まれる行を省く。
-#
-## sed s/inet/inet:/    -> ifconfigと違いip aには「:」が含まれない為、cutを使いにくいのでsedで置換をする。
-#
-## cut -d: -f2          -> cutを使い「:」をデリミタにし、ipアドレスの含まれる第二フィールドを選び「inet: 」を排除する。
-#
-## awk '{ print $1}'    -> cutで先頭部分を排除できたので、ipアドレスのある部分を引数「$1」とawkをつかって出力する。
-#
-###sed -e 's/\/24//g'   -> 「/24」部分を消す
-#
-##tr '.' '    '         -> bashでは文字列をスペースの位置で分割して複数の文字列のように扱うことが出来るので、「.」を「スペース」に置換する
-
-# ~$ ip a | grep 'inet' | grep /24 | grep -v 'inet6' | grep -v 'eth1' | grep -v '127.0.0.1' | sed s/inet/inet:/ | cut -d: -f2 | awk '{ print $1}'
-#~$ ip a | grep 'inet' | grep /24 | grep -v 'inet6' | grep -v 'eth1' | grep -v '127.0.0.1' | sed s/inet/inet:/ | cut -d: -f2 | awk '{ print $1}'
-# ->192.168.20.10/24
-#~$ ip a | grep 'inet' | grep /24 | grep -v 'inet6' | grep -v 'eth1' | grep -v '127.0.0.1' | sed s/inet/inet:/ | cut -d: -f2 | awk '{ print $1}' | sed -e 's/\/24//g' | tr '.' '    '
-# -> 192 168 20 10
 ip=$(ip a | grep 'inet' | grep /24 | grep -v 'inet6' | grep -v 'eth1' | grep -v '127.0.0.1' | sed s/inet/inet:/ | cut -d: -f2 | awk '{ print $1}' | sed -e 's/\/24//g' | tr '.' '    ')
-
-
 
 ##参考
 #変数展開時の単語分割（word split）をマスターする
@@ -59,21 +17,6 @@ function echo_4th {
     echo $4
 }
 
-##  192.168.20.100/24
-## だとしたら、
-# echo_1st $ip　追記で第一オクテット
-# echo_3rd $ip　追記で第三オクテット
-#この情報を下のループで回すようにする
-
-## せっかくprefixもついてるので有効活用できると尚良い？
-##               =>ハードルがあがるので今回は無しということに。上述した/24で絞る理由がこれ。
-
-#まずインターネットに繋がるかどうか確認(googleDNSに向けて)
-ping -c 5 8.8.8.8
-
-#これ以降をループさせる
-# 第４オクテット以外を、最初に抽出したものから引っ張りたい
-
 ####whileループ
 COUNT=0
 MAX_COUNT=255
@@ -82,17 +25,6 @@ do
         COUNT=`expr $COUNT + 1`
         ping -c `echo_1st $ip`.`echo_2nd $ip`.`echo_3rd $ip`.`echo "$COUNT"`
 done
-
-#####=========残骸=========
-####forループ
-#$COUNT=0
-                        #MAX_COUNT=255
-#for i in 1 2 3 4 5 6 7 8 9
-#do
-        #まちがいあり
-#        COUNT=`expr $COUNT + $i`
-#        ping -c 5 192.168.100.`echo "$COUNT"`
-#done
 
 ##正常終了を宣言
 exit 0
