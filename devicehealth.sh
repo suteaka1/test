411418154cb4ba9ea32477c9b9d55aae04c6c9c4
suteaka1 <butterext+github@gmail.com>
2015-05-20 16:30:08 +0900

    Update devicehealth.sh

--- devicehealth.sh
+++ devicehealth.sh
@@ -1,15 +1,12 @@
 #!/bin/sh
 #pinglog.txt is https://gist.github.com/suteaka1/1440b2a2f354fac21654
+cd ~/
 logfile=cat ~/pinglog.txt
-jointemp1=$(mktemp jointemp1.XXXX)
-jointemp2=$(mktemp jointemp2.XXXX)
-
-grep "statistics" $logfile >> $jointemp1
-
-grep "% packet loss" $logfile >> $jointemp2
-
-paste $jointemp1 $jointemp2 > jointemp3
-
+touch jointemp1
+touch jointemp2
+grep "statistics" $logfile >> jointemp1
+grep "% packet loss" $logfile >> jointemp2
+paste jointemp1 jointemp2 > jointemp3
 
 ###
 #つながるやつ
