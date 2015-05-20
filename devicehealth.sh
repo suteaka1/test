42aefc15fb4a52ce170a7acc2823a2db90a4063c
suteaka1 <butterext+github@gmail.com>
2015-05-20 15:44:20 +0900

    Update devicehealth.sh
    1行1行を処理しながらループさせるのはやめた
    ipアドレスとステータスのそれぞれでgrepしてから行番号一致などでjoinなどしてから正誤表を作る

--- devicehealth.sh
+++ devicehealth.sh
@@ -1,4 +1,19 @@
 #!/bin/sh
-
 #pinglog.txt is https://gist.github.com/suteaka1/1440b2a2f354fac21654
-echo $(grep "statistics" pinglog.txt) \n echo $(grep "% packet loss" pinglog.txt)
+logfile=cat ~/pinglog.txt
+jointemp1=$(mktemp jointemp1.XXXX)
+jointemp2=$(mktemp jointemp2.XXXX)
+
+grep "statistics" $logfile >> $jointemp1
+
+grep "% packet loss" $logfile >> $jointemp2
+
+paste $jointemp1 $jointemp2 > jointemp3
+
+
+###
+#つながるやつ
+cat jointemp3 | grep -v "100% packet loss"
+
+#つながらないやつ
+cat jointemp3 | grep "100% packet loss"
