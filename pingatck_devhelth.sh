22548f0dff52e2d93bbb28ce368648b002c4306b
suteaka1 <butterext+github@gmail.com>
2015-05-21 17:38:15 +0900

    Update pingatck_devhelth.sh
    ひとまず簡略化したものが動くかチェック

--- pingatck_devhelth.sh
+++ pingatck_devhelth.sh
@@ -10,28 +10,6 @@
 ###・pingして応答するかしないかを判断する条件式を付与する       -> sleepの必要性    ==>　不要
 ###・最後に結果を出力して正規表現で整形する？                   -> もっと高度なテキスト処理と、cronでまわすことで定期的なログを取れる？
 
-##参考
-# 変数展開時の単語分割（word split）をマスターする
-# http://qiita.com/uasi/items/82b7708d5da213ba7c31
-
-# 第1引数を echo する関数
-function echo_1st {
-    echo $1
-}
-# 第2引数を echo する関数
-function echo_2nd {
-    echo $2
-}
-# 第3引数を echo する関数
-function echo_3rd {
-    echo $3
-}
-# 第4引数を echo する関数
-function echo_4th {
-    echo $4
-}
-
-
 ####パイプ間のコマンドの説明
 ## ip a                 -> ifconfigなどで使われるnet-toolsは長い間メンテナンスされていなかったということで大変危険なので
 ##                      (理由:http://iwashi.co/2014/08/07/reason-why-we-should-use-iproute2/ )
@@ -56,10 +34,9 @@ function echo_4th {
 #
 ## tr '.' '    '         -> bashでは文字列をスペースの位置で分割して複数の文字列のように扱うことが出来るので、「.」を「スペース」に置換する
 
-ip=`ip a | grep 'inet' | grep /24 | grep -v 'inet6' | grep -v 'eth1' | grep -v '127.0.0.1' | sed s/inet/inet:/ | cut -d: -f2 | awk '{ print $1}' | sed -e 's/\/24//g' | tr '.' '    '`
-
-# 192.168.100.1が割り当てられたアドレスならば、「192.168.100」の第三オクテットまでを表示する
-ip3=`echo_1st $ip`.`echo_2nd $ip`.`echo_3rd $ip`
+ip1=`ip a | grep 'inet' | grep /24 | grep -v 'inet6' | grep -v 'eth1' | grep -v '127.0.0.1' | awk '{ print $2}' | sed -e 's/\/24//g' | tr '.' '    ' | awk '{ print $1}'`
+ip2=`ip a | grep 'inet' | grep /24 | grep -v 'inet6' | grep -v 'eth1' | grep -v '127.0.0.1' | awk '{ print $2}' | sed -e 's/\/24//g' | tr '.' '    ' | awk '{ print $2}'`
+ip3=`ip a | grep 'inet' | grep /24 | grep -v 'inet6' | grep -v 'eth1' | grep -v '127.0.0.1' | awk '{ print $2}' | sed -e 's/\/24//g' | tr '.' '    ' | awk '{ print $3}'`
 
 # 192.168.100.x の x 部分を0から255の順に試す
 COUNT=0
@@ -67,9 +44,20 @@ MAX_COUNT=255
 while [ $COUNT -lt $MAX_COUNT ]
 do
         COUNT=`expr $COUNT + 1`
-        ping -c 5 `echo "$ip3"`.`echo "$COUNT"`
+        ping -c 5 `echo "$ip1"`.`echo "$ip2"`.`echo "$ip3"`.`echo "$COUNT"`
         # -c[count] 5一つで約5秒かかるが、sleepしなくても順序は飛ばさない模様？
         # sleep 5
 done
 
 exit 0
+
+####課題
+#1.いらない処理が多いので削って簡素化する
+#2.devicehealth.shを組み込む。そのために、pingattack.shのループ内に組み込む
+#3.waitなりsleepなりをいれる
+#4.全体が終了するのに時間がかかる
+##早くするには以下がある
+## $ sudo ping -f -c 500 192.168.200.1
+##か
+## $ sudo ping -i 0.05 -c 500 192.168.200.1
+#があるがどっちもroot権限が必要なのがちょっと・・・
