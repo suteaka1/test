6ed7f59b043dfc3e7a4a87814532c8cb1dcb23db
suteaka1 <butterext+github@gmail.com>
2015-05-27 10:58:18 +0900

    Update pingatck_devhelth.sh
    メールで頂いた内容

--- pingatck_devhelth.sh
+++ pingatck_devhelth.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
-# pingしたときの一時作業スペース
-cd /tmp
+temp1=$(mktemp /tmp/temp1.XXXX)
 
-temp1=$(mktemp temp1.XXXX)
+ip1=`ip a | grep inet | grep -v "127\.0\.0\.1" | awk '{print $2}' | sed 's/\/24//g' | sed 's/\./\t/g' | awk '{print $1}'`
+ip2=`ip a | grep inet | grep -v "127\.0\.0\.1" | awk '{print $2}' | sed 's/\/24//g' | sed 's/\./\t/g' | awk '{print $2}'`
+ip3=`ip a | grep inet | grep -v "127\.0\.0\.1" | awk '{print $2}' | sed 's/\/24//g' | sed 's/\./\t/g' | awk '{print $3}'`
 
-ip1=`ip a | grep 'inet' | grep /24 | grep -v 'inet6' | grep -v 'eth1' | grep -v '127.0.0.1' | awk '{ print $2}' | sed -e 's/\/24//g' | tr '.' '    ' | awk '{ print $1}'`
-ip2=`ip a | grep 'inet' | grep /24 | grep -v 'inet6' | grep -v 'eth1' | grep -v '127.0.0.1' | awk '{ print $2}' | sed -e 's/\/24//g' | tr '.' '    ' | awk '{ print $2}'`
-ip3=`ip a | grep 'inet' | grep /24 | grep -v 'inet6' | grep -v 'eth1' | grep -v '127.0.0.1' | awk '{ print $2}' | sed -e 's/\/24//g' | tr '.' '    ' | awk '{ print $3}'`
 
 COUNT=9
 MAX_COUNT=30
@@ -14,16 +12,16 @@ MAX_COUNT=30
 while [ $COUNT -lt $MAX_COUNT ]
 do
         COUNT=`expr $COUNT + 1`
-        `echo $ip1`.`echo $ip2`.`echo $ip3`.`echo $COUNT`
-        ping -c 4 -w 3 `echo "$ip1"`.`echo "$ip2"`.`echo "$ip3"`.`echo "$COUNT"` > $temp1
-        judge=`grep -v "100% packet loss" $temp1
-       if [ $temp1 -eq 0 ];then
-                sed -e 's/^/x/g'
+#        echo "start ping to ${ip1}.${ip2}.${ip3}.${COUNT}"
+        ping -c 4 -w 4 ${ip1}.${ip2}.${ip3}.${COUNT} > $temp1
+        hantei=`grep "100% packet loss" $temp1 | wc -l`
+        if [ $hantei -eq 1 ] ; then
+         echo "X ${ip1}.${ip2}.${ip3}.${COUNT}"
         else
-                sed -e 's/^/o/g'
+         echo "O ${ip1}.${ip2}.${ip3}.${COUNT}"
         fi
 done
 
-rm /tmp/$temp
+rm -f /tmp/$temp1
 
 exit 0
