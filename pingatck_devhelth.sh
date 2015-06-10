11f94e95c404360cb7391577a053ae94a6c865e0
suteaka1 <butterext+github@gmail.com>
2015-06-10 16:23:54 +0900

    Update pingatck_devhelth.sh
    達成済み
    ・要望のあったiprouteコマンドからifconfigコマンドでオクテットの値を持ってこれるようにした(…)
    
    課題
    ・第四オクテットが0の場合と、255の場合の例外処理を作成できていない。
    ・目的のものとして正しいのかどうか、御三方にチェックして貰う。これをしないとまた出戻り…
    ・draw.ioで作ったフローチャートに反映させる。

--- pingatck_devhelth.sh
+++ pingatck_devhelth.sh
@@ -1,15 +1,53 @@
 #!/bin/sh
-ip1=`ip a | grep 'inet' | grep 'eth0' | awk '{ print $2}' | sed -e 's/\/24//g' | tr '.' '    ' | awk '{ print $1}'`
-ip2=`ip a | grep 'inet' | grep 'eth0' | awk '{ print $2}' | sed -e 's/\/24//g' | tr '.' '    ' | awk '{ print $2}'`
-ip3=`ip a | grep 'inet' | grep 'eth0' | awk '{ print $2}' | sed -e 's/\/24//g' | tr '.' '    ' | awk '{ print $3}'`
-ip4=`ip a | grep 'inet' | grep 'eth0' | awk '{ print $2}' | sed -e 's/\/24//g' | tr '.' '    ' | awk '{ print $4}'`
+
+ip1=`ifconfig | grep -A 1 'eth0' | grep 'inet' | awk '{ print $2}' | sed -e 's/addr://g'| tr '.' '    ' | awk '{ print $1}'`
+ip2=`ifconfig | grep -A 1 'eth0' | grep 'inet' | awk '{ print $2}' | sed -e 's/addr://g'| tr '.' '    ' | awk '{ print $2}'`
+ip3=`ifconfig | grep -A 1 'eth0' | grep 'inet' | awk '{ print $2}' | sed -e 's/addr://g'| tr '.' '    ' | awk '{ print $3}'`
+ip4=`ifconfig | grep -A 1 'eth0' | grep 'inet' | awk '{ print $2}' | sed -e 's/addr://g'| tr '.' '    ' | awk '{ print $4}'`
+
+
+#ip1=`ip a | grep 'inet' | grep 'eth0' | awk '{ print $2}' | sed -e 's/\/24//g' | tr '.' '    ' | awk '{ print $1}'`
+#ip2=`ip a | grep 'inet' | grep 'eth0' | awk '{ print $2}' | sed -e 's/\/24//g' | tr '.' '    ' | awk '{ print $2}'`
+#ip3=`ip a | grep 'inet' | grep 'eth0' | awk '{ print $2}' | sed -e 's/\/24//g' | tr '.' '    ' | awk '{ print $3}'`
+#ip4=`ip a | grep 'inet' | grep 'eth0' | awk '{ print $2}' | sed -e 's/\/24//g' | tr '.' '    ' | awk '{ print $4}'`
+
+#COUNT=`echo " ${ip4} - 6" | bc`
+#MAX_COUNT=`echo " ${ip4} + 5" | bc`
+
+        #if [ ${ip4} -eq 0 ]
+        #then
+        #        MAX_COUNT=`echo "${ip4} + 11" | bc`
+        #elif [ ${ip4} -eq 255 ]
+        #then
+        #        COUNT=`echo "${ip4} - 11" | bc`
+        #else
+        #        : #echo "例外です。シェルスクリプトを終了します。" ; exit 1
+        #fi
+while [ $COUNT -lt $MAX_COUNT ]
+        #if [ ${ip4} -eq 0 ]
+        #then
+        #       MAX_COUNT=`echo "${ip4} + 11" | bc`
+        #elif [ ${ip4} -eq 255 ]
+        #then
+        #       COUNT=`echo "${ip4} - 11" | bc`
+        #else
+        #       echo "例外です。シェルスクリプトを終了します。" ; exit 1
+        #fi
 
 COUNT=`echo " ${ip4} - 6" | bc`
 MAX_COUNT=`echo " ${ip4} + 5" | bc`
-
-while [ $COUNT -lt $MAX_COUNT ]
 do
         COUNT=`expr $COUNT + 1`
+
+        #if [ ${COUNT} = 0 ]
+        #then
+        #       MAX_COUNT=`echo "${COUNT} + 11" | bc`
+        #elif [ ${COUNT} = 255 ]
+        #then
+        #       COUNT=`echo "${COUNT} - 11" | bc`
+        #else
+        #       : #echo "例外です。シェルスクリプトを終了します。" ; exit 1
+        #fi
         ipaddr=${ip1}.${ip2}.${ip3}.${COUNT}
         judge=`ping -w 3 $ipaddr | grep '100%' | wc -l`
         if [ $judge -eq 1 ]
